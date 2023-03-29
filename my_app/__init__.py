44a7340c015b3eff66674a21006acd9bc1ae69a4
eugeneaondo <eugeneaondo11@gmail.com>
2023-03-29 17:56:41 +0300

    user_authentication

--- my_app/__init__.py
+++ my_app/__init__.py
@@ -8,12 +8,16 @@ def create_app():
     app.config.from_object(
     config['development'])
   
+    from my_app.auth.views import auth
+    app.register_blueprint(auth)
 
     from my_app.products.views import product
     app.register_blueprint(product)
     
     
     db.init_app(app)
+
+    db.create_all()
 
     return app
 
