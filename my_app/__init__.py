c557788c1c5af6c61a0ab2b0d3480b5b93e4526c
eugeneaondo <eugeneaondo11@gmail.com>
2023-03-31 03:05:42 +0300

    changes

--- my_app/__init__.py
+++ my_app/__init__.py
@@ -3,11 +3,12 @@ from flask_sqlalchemy import SQLAlchemy
 from .config import config
 db=SQLAlchemy()
 
-def create_app():
+
+def create_app(config_name):
     app = Flask(__name__)
-    app.config.from_object(
-    config['development'])
-  
+    app.config.from_object(config[config_name])
+    config[config_name].init_app(app)
+    
     from my_app.auth.views import auth
     app.register_blueprint(auth)
 
